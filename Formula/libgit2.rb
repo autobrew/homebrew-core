72b26411e374b5514fa4bcff808b8aa0254a3a9c
Jeroen Ooms <jeroenooms@gmail.com>
2020-10-13 14:34:18 +0200

    libgit2 1.1.0 (#29)
    * libgit2 1.1.0
    
    * update bottles

--- Formula/libgit2.rb
+++ Formula/libgit2.rb
@@ -1,14 +1,15 @@
 class Libgit2 < Formula
   desc "C library of Git core methods that is re-entrant and linkable"
   homepage "https://libgit2.github.com/"
-  url "https://github.com/libgit2/libgit2/archive/v1.0.0.tar.gz"
-  sha256 "6a1fa16a7f6335ce8b2630fbdbb5e57c4027929ebc56fcd1ac55edb141b409b4"
+  url "https://github.com/libgit2/libgit2/archive/v1.1.0.tar.gz"
+  sha256 "41a6d5d740fd608674c7db8685685f45535323e73e784062cf000a633d420d1e"
   head "https://github.com/libgit2/libgit2.git"
 
   bottle do
     cellar :any
     root_url "https://autobrew.github.io/bottles"
-    sha256 "42ae13158dcd1838c7db66196700dc24701a6663d2a22f98215023319a738d51" => :el_capitan
+    sha256 "19ce34c30736a66d4be9d002214202901f290e6a26aa7b77bff7112347b04d4b" => :el_capitan
+    sha256 "390785801ee168b094366836efc5db5b960160a35a65fc71666a5e87461130d3" => :high_sierra
   end
 
   depends_on "cmake" => :build
