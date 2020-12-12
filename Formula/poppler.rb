b167553ed0b484544f67891c2261e896626b5311
Jeroen Ooms <jeroenooms@gmail.com>
2020-12-12 22:00:59 +0100

    poppler 20.12.1
    update font-data

--- Formula/poppler.rb
+++ Formula/poppler.rb
@@ -1,14 +1,15 @@
 class Poppler < Formula
   desc "PDF rendering library (based on the xpdf-3.0 code base)"
   homepage "https://poppler.freedesktop.org/"
-  url "https://poppler.freedesktop.org/poppler-0.73.0.tar.xz"
-  sha256 "e44b5543903128884ba4538c2a97d3bcc8889e97ffacc4636112101f0238db03"
+  url "https://poppler.freedesktop.org/poppler-20.12.1.tar.xz"
+  sha256 "d0aa2586c0a4296c775f0d2045f28bb95a694113fc995f95350faa12930f7b35"
   head "https://anongit.freedesktop.org/git/poppler/poppler.git"
 
   bottle do
     cellar :any
     root_url "https://autobrew.github.io/bottles"
-    sha256 "6c659f3920d3a1915b9e54bcd153cf29b9203ecd1e701f78f6f3059313a9029b" => :el_capitan
+    sha256 "458214ac69c814d875fa77b9d6336106bbe5242e16bf1aa43ac8fb69bf399471" => :el_capitan
+    sha256 "468ca57507166a7c71a2a9503612d64d5ff74a554553ae8c727efde9343642ed" => :high_sierra
   end
 
   option "with-qt", "Build Qt5 backend"
@@ -35,8 +36,8 @@ class Poppler < Formula
     :because => "poppler, pdftohtml, pdf2image, and xpdf install conflicting executables"
 
   resource "font-data" do
-    url "https://poppler.freedesktop.org/poppler-data-0.4.9.tar.gz"
-    sha256 "1f9c7e7de9ecd0db6ab287349e31bf815ca108a5a175cf906a90163bdbe32012"
+    url "https://poppler.freedesktop.org/poppler-data-0.4.10.tar.gz"
+    sha256 "6e2fcef66ec8c44625f94292ccf8af9f1d918b410d5aa69c274ce67387967b30"
   end
 
 
@@ -52,12 +53,6 @@ class Poppler < Formula
       -DENABLE_NSS3=OFF
       -DWITH_GObjectIntrospection=OFF
     ]
-
-    if build.with? "qt"
-      args << "-DENABLE_QT5=ON"
-    else
-      args << "-DENABLE_QT5=OFF"
-    end
 
     system "cmake", ".", *args
     system "make", "install"
