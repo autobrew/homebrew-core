e6ede1534112dbdeb3474e128c93f23154b6f5a8
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-03 22:12:23 +0100

    update protobuf

--- Formula/protobuf.rb
+++ Formula/protobuf.rb
@@ -1,16 +1,15 @@
 class Protobuf < Formula
   desc "Protocol buffers (Google's data interchange format)"
   homepage "https://github.com/google/protobuf/"
-  url "https://github.com/google/protobuf/archive/v3.5.1.tar.gz"
-  sha256 "826425182ee43990731217b917c5c3ea7190cfda141af4869e6d4ad9085a740f"
-  revision 1
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v3.12.4/protobuf-all-3.12.4.tar.gz"
+  sha256 "e7bf41873d1a87c05c2b0a6197f4445c6ea3469ce0165ff14de2df8b34262530"
   head "https://github.com/google/protobuf.git"
 
   bottle do
-    sha256 "89d3e4a62799951c2a908f102ed305691f0fd0141b27c4337ef9bfe64840d8a9" => :high_sierra
-    sha256 "917abbf787422c4702b3104f5f6fb77f48dae573284f5aa7a9a2ef53793e5834" => :sierra
-    sha256 "5a0956aa0639b5943bee597942e7c0ab1439f2db6e322423a72a5ad68e28af82" => :el_capitan
-    root_url "https://homebrew.bintray.com/bottles"
+    cellar :any
+    root_url "https://autobrew.github.io/bottles"
+    sha256 "6b0b5e1d6f7d0daebaad1ca3f11dc037bc76459ce252e9c1d28674209bc2841c" => :high_sierra
+    sha256 "68d29d7911aaceba4b79237f086672710fa5368adb68e500a0a96094a6ce969f" => :el_capitan
   end
 
   # this will double the build time approximately if enabled
@@ -28,8 +27,8 @@ class Protobuf < Formula
   depends_on "python" => :optional
 
   resource "six" do
-    url "https://files.pythonhosted.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"
-    sha256 "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"
+    url "https://files.pythonhosted.org/packages/6b/34/415834bfdafca3c5f451532e8a8d9ba89a21c9743a0c59fbd0205c7f9426/six-1.15.0.tar.gz"
+    sha256 "30639c035cdb23534cd4aa2dd52c3bf48f06e5f4a941509c8bafd8ce11080259"
   end
 
   # Upstream's autogen script fetches this if not present
@@ -51,7 +50,7 @@ class Protobuf < Formula
     system "./autogen.sh"
 
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--with-zlib"
+                          "--prefix=#{prefix}", "--disable-shared", "--with-zlib"
     system "make"
     system "make", "check" if build.with?("test") || build.bottle?
     system "make", "install"
