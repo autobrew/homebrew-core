d11edd2b2971da00f28d8209c7a00556e6b0fd67
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-04 22:46:33 +0100

    assuan 2.5.4

--- Formula/libassuan.rb
+++ Formula/libassuan.rb
@@ -1,16 +1,15 @@
 class Libassuan < Formula
   desc "Assuan IPC Library"
   homepage "https://www.gnupg.org/related_software/libassuan/"
-  url "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.1.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libassuan/libassuan-2.5.1.tar.bz2"
-  sha256 "47f96c37b4f2aac289f0bc1bacfa8bd8b4b209a488d3d15e2229cb6cc9b26449"
+  url "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.4.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libassuan/libassuan-2.5.4.tar.bz2"
+  sha256 "c080ee96b3bd519edd696cfcebdecf19a3952189178db9887be713ccbcb5fbf0"
 
   bottle do
     cellar :any
-#    sha256 "3734fe43c829fd0586e71c1f458a87d5485bdef24db4d63541995d0ff1d8495e" => :mojave
-    sha256 "a05af2506efccbd153b216c2ed6b17c7b28b1c50fce16489166782877695b8ee" => :high_sierra
-    sha256 "3aefafdbaf62a8319b618c73654aef88f9ebe8b9cba51a304aebaa09bfc92c4c" => :sierra
-    sha256 "6e4ef55130d531e2e24226379f70c6ea59e62caf4fcc8fabffdecf8b2a0d018e" => :el_capitan
+    root_url "https://autobrew.github.io/bottles"
+    sha256 "e0cb226b596b8a021b026b4a1e357949fe52763d9fb512092df98fcacf16dd97" => :el_capitan
+    sha256 "c70981039e3febf01c6966ae8888942e0cbcd1ca6954c6b81804f306026cda68" => :high_sierra
   end
 
   depends_on "libgpg-error"
