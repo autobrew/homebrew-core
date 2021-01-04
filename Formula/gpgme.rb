5ced88bf48a2faae9e6b422ab926893e69ca0380
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-04 23:25:54 +0100

    gpgme 1.15

--- Formula/gpgme.rb
+++ Formula/gpgme.rb
@@ -1,15 +1,13 @@
 class Gpgme < Formula
   desc "Library access to GnuPG"
   homepage "https://www.gnupg.org/related_software/gpgme/"
-  url "https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.12.0.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gpgme/gpgme-1.12.0.tar.bz2"
-  sha256 "b4dc951c3743a60e2e120a77892e9e864fb936b2e58e7c77e8581f4d050e8cd8"
+  url "https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.15.0.tar.bz2"
+  sha256 "0b472bc12c7d455906c8a539ec56da0a6480ef1c3a87aa5b74d7125df68d0e5b"
 
   bottle do
-    cellar :any
-    rebuild 1
     root_url "https://autobrew.github.io/bottles"
-    sha256 "74b6fbc400e6b513cd42cfe8ab2379097dea11dadeb22b814d940c49e9ee06df" => :el_capitan
+    sha256 "39236fd4ad9d2ee1e3e535afe945133706f1b4133371e459c61d033b990310c4" => :el_capitan
+    sha256 "b1041937d74e7911fdb683b27d24e7c0e2d216433591907520658531e2931c93" => :high_sierra
   end
 
   depends_on "swig" => :build
