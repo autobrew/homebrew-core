efed339e8662d13e4d94b31fea1d6ba82e93920d
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-12 15:09:43 +0100

    rebuild imagemagick with libheif and libraw Closes #54

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -4,16 +4,16 @@ class ImagemagickAT6 < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.11-32.tar.xz"
-  mirror "https://www.imagemagick.org/download/releases/ImageMagick-6.9.11-32.tar.xz"
-  sha256 "151733e004cb1b49c77e04e1257b9883b6cce2221fc44b0176845e14f75eca52"
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.11-57.tar.xz"
+  mirror "https://www.imagemagick.org/download/releases/ImageMagick-6.9.11-57.tar.xz"
+  sha256 "1a1d35a6e702a498d34b4a4f9fbf5aab228ee233d18b83f742163071fc6b7e05"
   head "https://github.com/imagemagick/imagemagick6.git"
 
   bottle do
     cellar :any
     root_url "https://autobrew.github.io/bottles"
-    sha256 "556d15db80b11f70e1d48c593fb9cc99f92b4e535e770b077458d0098144b673" => :el_capitan
-    sha256 "50fdebe7f8be8c42a59d273ef6d91979129ac6e43bcc4047d42bf0a44638c59f" => :high_sierra
+    sha256 "01c692862ff5b2fc4e205ed30d83748303930df48bef3d61962f6acf990639d7" => :el_capitan
+    sha256 "f650cbe2d8269783c1c0954fcf924d278f6af0b88a1652048d9b5532347fe982" => :high_sierra
   end
 
   keg_only :versioned_formula
@@ -23,7 +23,9 @@ class ImagemagickAT6 < Formula
   depends_on "libtool"
   depends_on "xz"
   depends_on "jpeg"
+  depends_on "libheif"
   depends_on "libpng"
+  depends_on "libraw"
   depends_on "libtiff"
   depends_on "freetype"
   depends_on "fontconfig"
@@ -46,6 +48,8 @@ class ImagemagickAT6 < Formula
       --with-webp
       --with-pango
       --with-rsvg
+      --with-libraw
+      --with-heic
       --without-x
       --without-wmf
       --without-modules
@@ -58,7 +62,7 @@ class ImagemagickAT6 < Formula
     ]
 
     # versioned stuff in main tree is pointless for us
-    inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_VERSION}", "${PACKAGE_NAME}"
+    inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_BASE_VERSION}", "${PACKAGE_NAME}"
     system "./configure", *args
     system "make", "install"
   end
@@ -67,7 +71,7 @@ class ImagemagickAT6 < Formula
     assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
     # Check support for recommended features and delegates.
     features = shell_output("#{bin}/convert -version")
-    %w[fontconfig pango cairo rsvg webp freetype jpeg jp2 png tiff].each do |feature|
+    %w[raw heic fontconfig pango cairo rsvg webp freetype jpeg jp2 png tiff].each do |feature|
       assert_match feature, features
     end
   end
