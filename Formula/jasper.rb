f2e1e9d8a5ed8a64038d81fb5394c97cca62e3d9
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-12 21:07:33 +0100

    add static jasper

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -1,15 +1,8 @@
 class Jasper < Formula
   desc "Library for manipulating JPEG-2000 images"
   homepage "https://www.ece.uvic.ca/~frodo/jasper/"
-  url "https://github.com/mdadams/jasper/archive/version-2.0.14.tar.gz"
-  sha256 "85266eea728f8b14365db9eaf1edc7be4c348704e562bb05095b9a077cf1a97b"
-
-  bottle do
-#    sha256 "799c0986353251ad4b1d6d8cbdcfae357ef428cbc534a89ed0be5a5de3dcaf80" => :mojave
-    sha256 "086de22e8e8a01299962f3bea5374c90490b66e84b7e10a4078f172e64b0079f" => :high_sierra
-    sha256 "86058296fb5efea3ca509bd335bf7da48a83078fc237c1ccb1bb2d428ef2343b" => :sierra
-    sha256 "c481b8887b8d29e3c63735dd2151c9246e08f21bf50334033de4a054f700a6db" => :el_capitan
-  end
+  url "https://github.com/jasper-software/jasper/archive/version-2.0.24.tar.gz"
+  sha256 "d2d28e115968d38499163cf8086179503668ce0d71b90dd33855b3de96a1ca1d"
 
   depends_on "cmake" => :build
   depends_on "jpeg"
@@ -19,10 +12,21 @@ class Jasper < Formula
       # Make sure macOS's GLUT.framework is used, not XQuartz or freeglut
       # Reported to CMake upstream 4 Apr 2016 https://gitlab.kitware.com/cmake/cmake/issues/16045
       glut_lib = "#{MacOS.sdk_path}/System/Library/Frameworks/GLUT.framework"
-      system "cmake", "..", "-DGLUT_glut_LIBRARY=#{glut_lib}", *std_cmake_args
+
+      system "cmake", "..",
+        "-DGLUT_glut_LIBRARY=#{glut_lib}",
+        "-DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=false",
+        *std_cmake_args
       system "make"
-      system "make", "test"
       system "make", "install"
+      system "make", "clean"
+
+      system "cmake", "..",
+        "-DGLUT_glut_LIBRARY=#{glut_lib}",
+        "-DJAS_ENABLE_SHARED=OFF",
+        *std_cmake_args
+      system "make"
+      lib.install "src/libjasper/libjasper.a"
     end
   end
 
