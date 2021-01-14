fef4448e945374991b2144c69fa6c67ab860e69e
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-14 13:39:39 +0100

    Update freetype, closes #56

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -1,16 +1,15 @@
 class Freetype < Formula
   desc "Software library to render fonts"
   homepage "https://www.freetype.org/"
-  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.9.1/freetype-2.9.1.tar.bz2"
-  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.9.1.tar.bz2"
-  sha256 "db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d"
+  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.10.4/freetype-2.10.4.tar.xz"
+  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.10.4.tar.xz"
+  sha256 "86a854d8905b19698bbc8f23b860bc104246ce4854dcea8e3b0fb21284f75784"
 
   bottle do
     cellar :any
-#    sha256 "64d650278af1f74d43165f3943287b42109710e672d2756abaa492f0cc4d52b7" => :mojave
-    sha256 "444ef60a543aca6ca26223f46182c914e26d2908f33fca41cb54bcf9a81084a3" => :high_sierra
-    sha256 "4afc7a144563abc6f4e58ef45aa57da598051290e7568afbff028585717b30fa" => :sierra
-    sha256 "8f8fbbe028986cebd4a49c399e4861fb85cb6173298ba957172cb1ed915682ab" => :el_capitan
+    root_url "https://autobrew.github.io/bottles"
+    sha256 "c9bc84cf271b1875333b3bca3b1fc51414ec24331460634690432355c5c9d78a" => :high_sierra
+    sha256 "a9aa908276a422aa8f472510c0cb0dfdc3faa7cbaaf8e40dd46f77d2a2600158" => :el_capitan
   end
 
   depends_on "libpng"
