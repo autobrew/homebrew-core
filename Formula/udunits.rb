4f06f7d6c49fb85aee9afd00a0757d9d8024d336
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-16 17:05:48 +0100

    backport static libs for udunits

--- Formula/udunits.rb
+++ Formula/udunits.rb
@@ -3,9 +3,12 @@ class Udunits < Formula
   homepage "https://www.unidata.ucar.edu/software/udunits/"
   url "https://github.com/Unidata/UDUNITS-2/archive/v2.2.27.6.tar.gz"
   sha256 "74fd7fb3764ce2821870fa93e66671b7069a0c971513bf1904c6b053a4a55ed1"
+  revision 1
 
   bottle do
-    sha256 "3c12f59317ded4bdc6f89c24a0eec9260a499371c9c92b2d5e34c1b1a9f50a2c" => :high_sierra
+    cellar :any
+    root_url "https://autobrew.github.io/bottles"
+    sha256 "15a403c9e2aaf4062786fd0962e8cb44cfde23590c430c2aa0bbac465cbf2773" => :high_sierra
   end
 
   depends_on "cmake" => :build
@@ -16,6 +19,10 @@ class Udunits < Formula
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make", "install"
+      system "make", "clean"
+      system "cmake", "..", *std_cmake_args, "-DBUILD_SHARED_LIBS=OFF"
+      system "make"
+      lib.install "lib/libudunits2.a"
     end
   end
 
