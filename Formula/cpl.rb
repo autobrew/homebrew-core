766b286bcd0f660988c659cabe703ef62321916d
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-16 14:27:42 +0100

    add missing formula

---
+++ Formula/cpl.rb
@@ -0,0 +1,44 @@
+class Cpl < Formula
+  desc "ISO-C libraries for developing astronomical data-reduction tasks"
+  homepage "https://www.eso.org/sci/software/cpl/index.html"
+  url "ftp://ftp.eso.org/pub/dfs/pipelines/libraries/cpl/cpl-7.1.2.tar.gz"
+  mirror "https://src.fedoraproject.org/repo/pkgs/cpl/cpl-7.1.2.tar.gz/sha512/0835917153fdcf4e7908f9261e143dc54fe7b50dc7903c2041a23a1984554b94a99584def70a792d9fe44a42830bc2baee397635ce922848d514a580be59348a/cpl-7.1.2.tar.gz"
+  sha256 "b6d20752420e2333e86d9a08c24a08057351a9fef97c32f5894e63fbfece463a"
+  revision 6
+
+  bottle do
+    cellar :any
+    sha256 "7a810f03ee554b573832fd30285a2fad70be6b9ad6b0e9936300130396f151cd" => :high_sierra
+  end
+
+  depends_on "cfitsio"
+  depends_on "fftw"
+  depends_on "wcslib"
+
+  conflicts_with "gdal", because: "both install cpl_error.h"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--with-cfitsio=#{Formula["cfitsio"].prefix}",
+                          "--with-fftw=#{Formula["fftw"].prefix}",
+                          "--with-wcslib=#{Formula["wcslib"].prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOF
+      #include <cpl.h>
+      int main(){
+        cpl_init(CPL_INIT_DEFAULT);
+        cpl_msg_info("hello()", "Hello, world!");
+        cpl_end();
+        return 0;
+      }
+    EOF
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}", "-lcplcore", "-lcext", "-o", "test"
+    system "./test"
+  end
+end
