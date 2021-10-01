a4988fac992407e2f338146086fc82eadc00b91f
Jeroen Ooms <jeroenooms@gmail.com>
2021-10-01 13:18:13 +0200

    backport zstd

--- Formula/zstd.rb
+++ Formula/zstd.rb
@@ -1,32 +1,37 @@
 class Zstd < Formula
   desc "Zstandard is a real-time compression algorithm"
-  homepage "https://facebook.github.io/zstd/"
-  url "https://github.com/facebook/zstd/archive/v1.4.5.tar.gz"
-  sha256 "734d1f565c42f691f8420c8d06783ad818060fc390dee43ae0a89f86d0a4f8c2"
+  homepage "http://zstd.net/"
+  url "https://github.com/facebook/zstd/releases/download/v1.5.0/zstd-1.5.0.tar.gz"
+  sha256 "5194fbfa781fcf45b98c5e849651aa7b3b0a008c6b72d4a0db760f3002291e94"
 
   bottle do
     cellar :any
-    sha256 "61de5a45183f4d029c66024d645ad44b0a625d58f9f583b47af42346a7c90fe5" => :high_sierra
+    sha256 "2380f8e4de1df9ab047a3acbdfe8b0c046ef4fc42bf64ada8d264ed5381c4255" => :high_sierra
+    sha256 "99cba2f60365eeb883dafd6b69ca4eb353014020a6fd3e979a377e56dd8d812f" => :el_capitan
+    root_url "https://autobrew.github.io/bottles"
   end
 
-  depends_on "cmake" => :build
+  option "without-pzstd", "Build without parallel (de-)compression tool"
 
-  #uses_from_macos "zlib"
+  depends_on "cmake" => :build
 
   def install
     system "make", "install", "PREFIX=#{prefix}/"
 
-    # Build parallel version
-    system "make", "-C", "contrib/pzstd", "googletest"
-    system "make", "-C", "contrib/pzstd", "PREFIX=#{prefix}"
-    bin.install "contrib/pzstd/pzstd"
+    if build.with? "pzstd"
+      system "make", "-C", "contrib/pzstd", "googletest"
+      system "make", "-C", "contrib/pzstd", "PREFIX=#{prefix}"
+      bin.install "contrib/pzstd/pzstd"
+    end
   end
 
   test do
     assert_equal "hello\n",
       pipe_output("#{bin}/zstd | #{bin}/zstd -d", "hello\n", 0)
 
-    assert_equal "hello\n",
-      pipe_output("#{bin}/pzstd | #{bin}/pzstd -d", "hello\n", 0)
+    if build.with? "pzstd"
+      assert_equal "hello\n",
+        pipe_output("#{bin}/pzstd | #{bin}/pzstd -d", "hello\n", 0)
+    end
   end
 end
