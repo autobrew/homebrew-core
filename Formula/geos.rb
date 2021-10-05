5371edcebcf74ef1951b045c0938be119f636e0a
Jeroen Ooms <jeroenooms@gmail.com>
2021-10-06 00:48:11 +0200

    geos 3.9.1

--- Formula/geos.rb
+++ Formula/geos.rb
@@ -1,25 +1,19 @@
 class Geos < Formula
   desc "Geometry Engine"
   homepage "https://trac.osgeo.org/geos"
-  url "https://download.osgeo.org/geos/geos-3.8.1.tar.bz2"
-  sha256 "4258af4308deb9dbb5047379026b4cd9838513627cb943a44e16c40e42ae17f7"
-  revision 2
+  url "https://download.osgeo.org/geos/geos-3.9.1.tar.bz2"
+  sha256 "7e630507dcac9dc07565d249a26f06a15c9f5b0c52dd29129a0e3d381d7e382a"
 
   bottle do
     cellar :any
-    sha256 "d34ea2e3316cf9e1afdd89932168285985a1ca5790ae996c6fdba2df11e5621a" => :high_sierra
+    root_url "https://autobrew.github.io/bottles"
+    sha256 "be0802809ba01d6508780f24ac5cba78d480be9e6a1f649be8979ff04d3aec63" => :high_sierra
   end
 
   depends_on "swig" => :build
   depends_on "python" => :build
 
   def install
-    # https://trac.osgeo.org/geos/ticket/771
-    inreplace "configure" do |s|
-      s.gsub! /PYTHON_CPPFLAGS=.*/, %Q(PYTHON_CPPFLAGS="#{`python3-config --includes`.strip}")
-      s.gsub! /PYTHON_LDFLAGS=.*/, 'PYTHON_LDFLAGS="-Wl,-undefined,dynamic_lookup"'
-    end
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--enable-python",
