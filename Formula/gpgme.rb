ae95db2ebcbdf1ad729b16cf29757be42ff4265f
Jeroen Ooms <jeroenooms@gmail.com>
2021-11-17 13:00:14 +0100

    gpgme 1.16.0 (#88)

--- Formula/gpgme.rb
+++ Formula/gpgme.rb
@@ -1,13 +1,13 @@
 class Gpgme < Formula
   desc "Library access to GnuPG"
   homepage "https://www.gnupg.org/related_software/gpgme/"
-  url "https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.15.0.tar.bz2"
-  sha256 "0b472bc12c7d455906c8a539ec56da0a6480ef1c3a87aa5b74d7125df68d0e5b"
+  url "https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.16.0.tar.bz2"
+  sha256 "6c8cc4aedb10d5d4c905894ba1d850544619ee765606ac43df7405865de29ed0"
 
   bottle do
     root_url "https://autobrew.github.io/bottles"
-    sha256 "39236fd4ad9d2ee1e3e535afe945133706f1b4133371e459c61d033b990310c4" => :el_capitan
-    sha256 "b1041937d74e7911fdb683b27d24e7c0e2d216433591907520658531e2931c93" => :high_sierra
+    sha256 "6704e3bee444a39a3165cebebd3d55bcde6d60d0768d1b4c1a60cfe0af8c31a3" => :el_capitan
+    sha256 "e9c2977cd2624eab71e501913f49471534f2edef66d0b36df9ced7ada2efb795" => :high_sierra
   end
 
   depends_on "swig" => :build
@@ -15,11 +15,19 @@ class Gpgme < Formula
   depends_on "libassuan"
   depends_on "libgpg-error"
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
-                          "--enable-static"
+                          "--enable-static",
+                          "--disable-gpgconf-test",
+                          "--disable-gpg-test"
     system "make"
     system "make", "install"
 
