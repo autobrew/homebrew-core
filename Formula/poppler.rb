d2d8775c91d033bdf3ddcaaf97fd8bd3ef36bb45
Jeroen Ooms <jeroenooms@gmail.com>
2022-02-15 14:52:29 +0100

    poppler 20.12.1

--- Formula/poppler.rb
+++ Formula/poppler.rb
@@ -1,15 +1,15 @@
 class Poppler < Formula
   desc "PDF rendering library (based on the xpdf-3.0 code base)"
   homepage "https://poppler.freedesktop.org/"
-  url "https://poppler.freedesktop.org/poppler-20.12.1.tar.xz"
-  sha256 "d0aa2586c0a4296c775f0d2045f28bb95a694113fc995f95350faa12930f7b35"
-  head "https://anongit.freedesktop.org/git/poppler/poppler.git"
+  url "https://poppler.freedesktop.org/poppler-21.12.0.tar.xz"
+  sha256 "acb840c2c1ec07d07e53c57c4b3a1ff3e3ee2d888d44e1e9f2f01aaf16814de7"
+  head "https://gitlab.freedesktop.org/poppler/poppler.git"
 
   bottle do
     cellar :any
     root_url "https://autobrew.github.io/bottles"
-    sha256 "458214ac69c814d875fa77b9d6336106bbe5242e16bf1aa43ac8fb69bf399471" => :el_capitan
-    sha256 "468ca57507166a7c71a2a9503612d64d5ff74a554553ae8c727efde9343642ed" => :high_sierra
+    sha256 "78a3e3c058b47e47aa2585313c9f2a1545481488bd544a18ff054cceeefb510a" => :el_capitan
+    sha256 "eef6ee40c7ed637eeef1885b05ea25deb06da159d23961773c4e6f45a57be545" => :high_sierra
   end
 
   option "with-qt", "Build Qt5 backend"
@@ -36,19 +36,20 @@ class Poppler < Formula
     :because => "poppler, pdftohtml, pdf2image, and xpdf install conflicting executables"
 
   resource "font-data" do
-    url "https://poppler.freedesktop.org/poppler-data-0.4.10.tar.gz"
-    sha256 "6e2fcef66ec8c44625f94292ccf8af9f1d918b410d5aa69c274ce67387967b30"
+    url "https://poppler.freedesktop.org/poppler-data-0.4.11.tar.gz"
+    sha256 "2cec05cd1bb03af98a8b06a1e22f6e6e1a65b1e2f3816cb3069bb0874825f08c"
   end
-
 
   def install
     ENV.cxx11 if build.with?("qt") || MacOS.version < :mavericks
 
     args = std_cmake_args + %w[
       -DBUILD_GTK_TESTS=OFF
+      -DENABLE_BOOST=OFF
       -DENABLE_CMS=lcms2
       -DENABLE_GLIB=ON
       -DENABLE_QT5=OFF
+      -DENABLE_QT6=OFF
       -DENABLE_UNSTABLE_API_ABI_HEADERS=ON
       -DENABLE_NSS3=OFF
       -DWITH_GObjectIntrospection=OFF
