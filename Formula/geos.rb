927245afc0b61a5341660a3dc33565f38618f613
Jeroen Ooms <jeroenooms@gmail.com>
2022-03-20 11:07:36 +0100

    geos 3.10.2

--- Formula/geos.rb
+++ Formula/geos.rb
@@ -1,8 +1,8 @@
 class Geos < Formula
   desc "Geometry Engine"
   homepage "https://trac.osgeo.org/geos"
-  url "https://download.osgeo.org/geos/geos-3.9.1.tar.bz2"
-  sha256 "7e630507dcac9dc07565d249a26f06a15c9f5b0c52dd29129a0e3d381d7e382a"
+  url "https://download.osgeo.org/geos/geos-3.10.2.tar.bz2"
+  sha256 "50bbc599ac386b4c2b3962dcc411f0040a61f204aaef4eba7225ecdd0cf45715"
 
   bottle do
     cellar :any
@@ -10,18 +10,46 @@ class Geos < Formula
     sha256 "be0802809ba01d6508780f24ac5cba78d480be9e6a1f649be8979ff04d3aec63" => :high_sierra
   end
 
-  depends_on "swig" => :build
-  depends_on "python" => :build
+  depends_on "cmake" => :build
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--enable-python",
-                          "PYTHON=#{Formula["python"].opt_bin}/python3"
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{lib}"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    system "cmake", "-S", ".", "-B", "static", *std_cmake_args, "-DBUILD_SHARED_LIBS=OFF"
+    system "cmake", "--build", "static"
+    lib.install Dir["static/lib/*.a"]
   end
 
   test do
-    system "#{bin}/geos-config", "--libs"
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <stdarg.h>
+      #include <geos_c.h>
+      static void geos_message_handler(const char* fmt, ...) {
+          va_list ap;
+          va_start(ap, fmt);
+          vprintf (fmt, ap);
+          va_end(ap);
+      }
+      int main() {
+          initGEOS(geos_message_handler, geos_message_handler);
+          const char* wkt_a = "POLYGON((0 0, 10 0, 10 10, 0 10, 0 0))";
+          const char* wkt_b = "POLYGON((5 5, 15 5, 15 15, 5 15, 5 5))";
+          GEOSWKTReader* reader = GEOSWKTReader_create();
+          GEOSGeometry* geom_a = GEOSWKTReader_read(reader, wkt_a);
+          GEOSGeometry* geom_b = GEOSWKTReader_read(reader, wkt_b);
+          GEOSGeometry* inter = GEOSIntersection(geom_a, geom_b);
+          GEOSWKTWriter* writer = GEOSWKTWriter_create();
+          GEOSWKTWriter_setTrim(writer, 1);
+          char* wkt_inter = GEOSWKTWriter_write(writer, inter);
+          printf("Intersection(A, B): %s\\n", wkt_inter);
+          return 0;
+      }
+    EOS
+    cflags = shell_output("#{bin}/geos-config --cflags").split
+    libs = shell_output("#{bin}/geos-config --clibs").split
+    system ENV.cc, *cflags, "test.c", *libs
+    assert_match "POLYGON ((10 10, 10 5, 5 5, 5 10, 10 10))", shell_output("./a.out")
   end
 end
