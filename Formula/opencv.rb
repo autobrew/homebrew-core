dab643fdf8b2f3a3ca4b59b7bef3d64897de9f7f
Jeroen Ooms <jeroenooms@gmail.com>
2022-04-28 20:07:29 +0200

    Need protobuf for DNN now

--- Formula/opencv.rb
+++ Formula/opencv.rb
@@ -16,6 +16,7 @@ class Opencv < Formula
   depends_on "jpeg"
   depends_on "libpng"
   depends_on "libtiff"
+  depends_on "protobuf"
   depends_on "webp"
   depends_on "python" => :build
   depends_on "tbb"
@@ -66,7 +67,7 @@ class Opencv < Formula
       -DWITH_QT=OFF
       -DWITH_TBB=ON
       -DWITH_VTK=OFF
-      -DWITH_PROTOBUF=OFF
+      -DWITH_PROTOBUF=ON
       -DWITH_QUIRC=OFF
       -DWITH_ADE=OFF
       -DWITH_IPP=OFF
