3f8d6a1d5e72a6d05c1150172f64c70aba7807ee
Jeroen Ooms <jeroenooms@gmail.com>
2022-09-05 13:20:23 +0200

    libssh2: build from git (#115)
    * libssh2: build from git
    
    * update bottle [ci skip]

--- Formula/libssh2.rb
+++ Formula/libssh2.rb
@@ -1,14 +1,14 @@
 class Libssh2 < Formula
   desc "C library implementing the SSH2 protocol"
   homepage "https://libssh2.org/"
-  url "https://libssh2.org/download/libssh2-1.9.0.tar.gz"
-  sha256 "d5fb8bd563305fd1074dda90bd053fb2d29fc4bce048d182f96eaa466dfadafd"
+  url "https://github.com/libssh2/libssh2.git",
+      :revision => "6c59eea5a9ea77127ec0fa3d6815c8adc743dba3"
+  version "1.10.1"
 
   bottle do
     cellar :any
     root_url "https://autobrew.github.io/bottles"
-    sha256 "f5e9abfe13cbea489ab11d5a7c5704d29b5c7c2f74921c32b1db908cc0c38a3b" => :high_sierra
-    sha256 "a0ab62111867ff1e87077ee8c82fd3fcde3d0d3a1771de4422eb05160486c973" => :el_capitan
+    sha256 "dfcbd5dd5dd25299fbc6c90065061d12e5b5a8b9caacce5249370a58b652922b" => :high_sierra
   end
 
   head do
@@ -19,6 +19,10 @@ class Libssh2 < Formula
     depends_on "libtool" => :build
   end
 
+  # Build from git for now: https://github.com/libssh2/libssh2/issues/536#issuecomment-892897873
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "openssl@1.1"
 
   def install
@@ -33,7 +37,7 @@ class Libssh2 < Formula
       --with-libssl-prefix=#{Formula["openssl@1.1"].opt_prefix}
     ]
 
-    system "./buildconf" if build.head?
+    system "autoreconf -fi"
     system "./configure", *args
     system "make", "install"
   end
