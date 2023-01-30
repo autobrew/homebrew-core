9d7215f6535a7c174f882949cdf38afb515caf71
Nic Crane <thisisnic@gmail.com>
2023-01-30 13:57:21 +0000

    apache-arrow 11.0.0 (#122)

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -1,14 +1,14 @@
 class ApacheArrow < Formula
   desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
-  url "https://downloads.apache.org/arrow/arrow-10.0.1/apache-arrow-10.0.1.tar.gz"
+  url "https://downloads.apache.org/arrow/arrow-11.0.0/apache-arrow-11.0.0.tar.gz"
   # Uncomment and update to test on a release candidate 
-  mirror "https://dist.apache.org/repos/dist/dev/arrow/apache-arrow-10.0.1-rc0/apache-arrow-10.0.1.tar.gz"
-  sha256 "c814e0670112a22c1a6ec03ab420a52ae236a9a42e9e438c3cbd37f37e658fb3"
+  mirror "https://dist.apache.org/repos/dist/dev/arrow/apache-arrow-11.0.0-rc0/apache-arrow-11.0.0.tar.gz"
+  sha256 "2dd8f0ea0848a58785628ee3a57675548d509e17213a2f5d72b0d900b43f5430"
 
   bottle do
     cellar :any
-    sha256 "785b58d1492459dc4868b154fca930b1d1a51e07d3f58d2912f82996c4b28fae" => :high_sierra
+    sha256 "27b3677ea434c01b1878f1d794d28332d0b5c58d9453841fe389645fc6bd4e8d" => :high_sierra
     root_url "https://autobrew.github.io/bottles"
   end
 
