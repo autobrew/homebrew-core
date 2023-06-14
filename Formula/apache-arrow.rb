7e216afbfbc41b51e865fa72378a2c2eab941ce8
Nic Crane <thisisnic@gmail.com>
2023-06-14 14:04:00 +0100

    apache-arrow 12.0.1 (#125)

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -1,10 +1,10 @@
 class ApacheArrow < Formula
   desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
-  url "https://downloads.apache.org/arrow/arrow-12.0.0/apache-arrow-12.0.0.tar.gz"
+  url "https://downloads.apache.org/arrow/arrow-12.0.1/apache-arrow-12.0.1.tar.gz"
   # Uncomment and update to test on a release candidate 
-  mirror "https://dist.apache.org/repos/dist/dev/arrow/apache-arrow-12.0.0-rc0/apache-arrow-12.0.0.tar.gz"
-  sha256 "ddd8347882775e53af7d0965a1902b7d8fcd0a030fd14f783d4f85e821352d52"
+  mirror "https://dist.apache.org/repos/dist/dev/arrow/apache-arrow-12.0.1-rc1/apache-arrow-12.0.1.tar.gz"
+  sha256 "55fe1d17a3c2121b39bd71d7fc61d1053b183fa3bb5982a76d3c2b3ac5fb4aca"
 
   bottle do
     cellar :any
